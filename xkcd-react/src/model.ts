0b1b3e721a9740ef61d902c436a6466301c180ac
Dejan laptop <dejan.katasic@gmail.com>
2021-04-26 21:26:09 +0200

    typescript

---
+++ xkcd-react/src/model.ts
@@ -0,0 +1,13 @@
+export class XkcdComic {
+    public alt?: string;
+    public day?: string;
+    public img?: string;
+    public link?: string;
+    public month?: string;
+    public news?: string;
+    public num?: number;
+    public safe_title?: string;
+    public title?: string;
+    public transcript?: string;
+    public year?: string;
+}
